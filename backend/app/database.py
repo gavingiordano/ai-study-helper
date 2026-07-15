92889dc97a42308f66a3bec7ad84e017279d0473
Gavin Giordano <gavingiordano18@gmail.com>
2026-07-14 20:21:09 -0400

    set up database

---
+++ backend/app/database.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from sqlmodel import SQLModel, create_engine, Session
+import os
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True
+)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
