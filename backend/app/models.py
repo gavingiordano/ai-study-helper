5ee6e9d96b36c062464fd297d27a249252762571
Gavin Giordano <gavingiordano18@gmail.com>
2026-07-21 21:00:05 -0400

    add created_at attribute for users, courses, and documents

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 from sqlmodel import Field, SQLModel
 
@@ -7,6 +8,7 @@ class User(SQLModel, table=True):
     name: Optional[str] = None
     email: str = Field(index=True, unique=True)
     hashed_password: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
 
 
 class Course(SQLModel, table=True):
@@ -14,6 +16,8 @@ class Course(SQLModel, table=True):
     user_id: int = Field(foreign_key="user.id")
     name: str
     code: Optional[str]  = None
+    description: Optional[str] = None
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
 
 
 class Document(SQLModel, table=True):
@@ -22,3 +26,4 @@ class Document(SQLModel, table=True):
     displayed_name: str
     original_filename: str
     file_path: str
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc), nullable=False)
